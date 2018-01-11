b0d578dbb2ed7a81c4ba039b69ee4e0b9d834aea
= <=>
2018-01-11 15:26:47 +0530

    Graph plotted

--- gdp_data.R
+++ gdp_data.R
@@ -1,9 +1,22 @@
 library(tidyverse)
+library(ggplot2)
 gdp_data <- read.csv("datafile.csv")
 
-gdp_share <- data.frame(gdp_data$Gross.Domestic.Product..in.Rs..Cr..at.2004.05,gdp_data$Agriculture...Allied.Services..in.Rs..Cr...at.2004.05.Prices,gdp_data$Agriculture..in.Rs..Cr...at.2004.05.Prices,gdp_data$Industry..in.Rs..Cr...at.2004.05.Prices,gdp_data$Mining.and.Quarrying..in.Rs..Cr...at.2004.05.Prices,gdp_data$Manufacturing..in.Rs..Cr...at.2004.05.Prices,gdp_data$Services..in.Rs..Cr...at.2004.05.Prices)
+gdp_test <- data.frame(gdp_data$Gross.Domestic.Product..in.Rs..Cr..at.2004.05,gdp_data$Agriculture...Allied.Services..in.Rs..Cr...at.2004.05.Prices,gdp_data$Agriculture..in.Rs..Cr...at.2004.05.Prices,gdp_data$Industry..in.Rs..Cr...at.2004.05.Prices,gdp_data$Mining.and.Quarrying..in.Rs..Cr...at.2004.05.Prices,gdp_data$Manufacturing..in.Rs..Cr...at.2004.05.Prices,gdp_data$Services..in.Rs..Cr...at.2004.05.Prices)
+names(gdp_test) <- c("Total_GDP","Agriculture_allied_services","Agriculture","Industry","Mining_and_quarrying","Manufacturing","Services")
+gdp_test <- gdp_test/gdp_test$Total_GDP
+gdp_test <- subset(gdp_test,select = -Total_GDP)
+gdp_test <- mutate(gdp_test,gdp_data$Financial.Year)
+colnames(gdp_test)[colnames(gdp_test) == "gdp_data$Financial.Year"] <- "Financial_year"
+
+gdp_share <-data.frame(gdp_data$Gross.Domestic.Product..in.Rs..Cr..at.2004.05,gdp_data$Agri.culture...Allied.Services..Share.to.Total.GDP,gdp_data$Agriculture...Share.to.Total.GDP ,gdp_data$Industry...Share.to.Total.GDP ,gdp_data$Mining.and.Quarrying...Share.to.Total.GDP,gdp_data$Manufacturing...Share.to.Total.GDP,gdp_data$Services...Share.to.Total.GDP)
 names(gdp_share) <- c("Total_GDP","Agriculture_allied_services","Agriculture","Industry","Mining_and_quarrying","Manufacturing","Services")
 gdp_share <- gdp_share/gdp_share$Total_GDP
-gdp_share <- subset(gdp_share,select = -Total_GDP)
+gdp_share <- subset(gdp_share,select = - Total_GDP)
+gdp_share <-mutate(gdp_share,1-rowSums(gdp_share))
+colnames(gdp_share)[colnames(gdp_share) == "1 - rowSums(gdp_share)"] <- "Others"
 gdp_share <- mutate(gdp_share,gdp_data$Financial.Year)
 colnames(gdp_share)[colnames(gdp_share) == "gdp_data$Financial.Year"] <- "Financial_year"
+plot_gdp <- melt(gdp_share,id.vars="Financial_year")
+#ggplot(plot_gdp, aes(x = Financial_year, y = value)) + geom_line(aes(color = variable,group = variable)) # To plot Financial year vs value
+
