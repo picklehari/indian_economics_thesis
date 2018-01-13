79b7e3747c28173b807bbcb3292fb2e4d06a19db
= <=>
2018-01-13 14:49:01 +0530

    Removed Others

--- gdp_data.R
+++ gdp_data.R
@@ -1,5 +1,7 @@
 library(tidyverse)
+library(reshape2)
 library(ggplot2)
+
 gdp_data <- read.csv("datafile.csv")
 
 gdp_test <- data.frame(gdp_data$Gross.Domestic.Product..in.Rs..Cr..at.2004.05,gdp_data$Agriculture...Allied.Services..in.Rs..Cr...at.2004.05.Prices,gdp_data$Agriculture..in.Rs..Cr...at.2004.05.Prices,gdp_data$Industry..in.Rs..Cr...at.2004.05.Prices,gdp_data$Mining.and.Quarrying..in.Rs..Cr...at.2004.05.Prices,gdp_data$Manufacturing..in.Rs..Cr...at.2004.05.Prices,gdp_data$Services..in.Rs..Cr...at.2004.05.Prices)
@@ -13,8 +15,8 @@ gdp_share <-data.frame(gdp_data$Gross.Domestic.Product..in.Rs..Cr..at.2004.05,gd
 names(gdp_share) <- c("Total_GDP","Agriculture_allied_services","Agriculture","Industry","Mining_and_quarrying","Manufacturing","Services")
 gdp_share <- gdp_share/gdp_share$Total_GDP
 gdp_share <- subset(gdp_share,select = - Total_GDP)
-gdp_share <-mutate(gdp_share,1-rowSums(gdp_share))
-colnames(gdp_share)[colnames(gdp_share) == "1 - rowSums(gdp_share)"] <- "Others"
+#gdp_share <-mutate(gdp_share,1-rowSums(gdp_share))
+#colnames(gdp_share)[colnames(gdp_share) == "1 - rowSums(gdp_share)"] <- "Others"
 gdp_share <- mutate(gdp_share,gdp_data$Financial.Year)
 colnames(gdp_share)[colnames(gdp_share) == "gdp_data$Financial.Year"] <- "Financial_year"
 plot_gdp <- melt(gdp_share,id.vars="Financial_year")
